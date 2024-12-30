069864a6f76e423cdd9e19eace0f1a4b06c5bb93
Haruman Wiguna <harumanwiguna1@gmail.com>
2024-12-30 20:01:59 +0700

    qq
    qq

--- Assets/Scripts/LoadingScreen.cs
+++ Assets/Scripts/LoadingScreen.cs
@@ -19,7 +19,7 @@ public class LoadingScreen : MonoBehaviour
 
     public void Start()
     {
-        // _loadingUI.SetActive(true);
+        _loadingUI.SetActive(true);
         loadingBarImage.fillAmount = 0f;
         buttonSkip.SetActive(false); // Pastikan buttonSkip disembunyikan saat awal
     }
